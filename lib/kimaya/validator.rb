9f19f875b5144b27cf220f62c612eb78f8e7aebf
Shailesh Patil <shailesh@joshsoftware.com>
2011-04-13 15:25:30 +0530

    bug fixes

--- lib/kimaya/validator.rb
+++ lib/kimaya/validator.rb
@@ -1,7 +1,7 @@
 module Kimaya
   class TPNValidator < ActiveModel::Validator
     def validate(record)
-      record.errors[:day_of_tpn] << "day_of_tpn.message" if record.day_of_tpn.blank?
+      record.errors[:day_of_tpn] << :day_of_tpn if record.day_of_tpn.blank?
     end
   end
 end
