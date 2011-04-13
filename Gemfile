8833bef91a6cda5fdc790d887196d8884001d305
Shailesh Patil <shailesh@joshsoftware.com>
2011-04-13 12:19:32 +0530

    including active_support

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "http://rubygems.org"
 
+gem 'activesupport'
 gem "activemodel"
 # Specify your gem's dependencies in kimaya.gemspec
 gemspec
--- lib/kimaya.rb
+++ lib/kimaya.rb
@@ -1,3 +1,4 @@
+require 'active_support'
 require 'active_model'
 require  File.dirname(__FILE__) + '/kimaya/kimaya_core.rb'
 require  File.dirname(__FILE__) + '/kimaya/kimaya.rb'
