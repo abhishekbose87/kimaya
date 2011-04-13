d9937aaf26b38612bfa53d49111171872538df86
Shailesh Patil <shailesh@joshsoftware.com>
2011-04-13 16:48:49 +0530

    bug fixes

--- lib/kimaya/validator.rb
+++ lib/kimaya/validator.rb
@@ -3,8 +3,8 @@ module Kimaya
     REQUIRED_FIELDS = %w{day_of_tpn current_weight percent_dextrose_conc total_fluid_intake fat_intake lipid_conc overfill_factor 
     amino_acid_intake amino_acid_conc administration}
     def validate(record)
+      record.errors.add(:day_of_tpn, :day_of_tpn_blank) if record.day_of_tpn.blank? 
       REQUIRED_FIELDS.each do |field|
-        record.errors.add("#{field}", :day_of_tpn) if eval("record.#{field}.blank?")
       end
     end
   end
