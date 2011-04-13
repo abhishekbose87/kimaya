d1e3d7b24303b1949a62d909acf15eb4e0695630
Shailesh Patil <shailesh@joshsoftware.com>
2011-04-13 14:06:56 +0530

    adding error messages

--- lib/kimaya/validator.rb
+++ lib/kimaya/validator.rb
@@ -1,7 +1,7 @@
 module Kimaya
   class TPNValidator < ActiveModel::Validator
     def validate(record)
-      record.errors[:base] << "This record is invalid"
+      record.errors[:day_of_tpn] << t('errors.1001.message') if record.day_of_tpn.blank?
     end
   end
 end
