44183845fb886a7eb48a6118ee1793c65438ac19
Shailesh Patil <shailesh@joshsoftware.com>
2011-04-13 15:35:09 +0530

    validations for the input fields

--- lib/kimaya/validator.rb
+++ lib/kimaya/validator.rb
@@ -1,7 +1,11 @@
 module Kimaya
   class TPNValidator < ActiveModel::Validator
+    REQUIRED_FIELDS = %w{day_of_tpn current_weight percent_dextrose_conc total_fluid_intake fat_intake lipid_conc overfill_factor 
+    amino_acid_intake amino_acid_conc administration}
     def validate(record)
-      record.errors[:day_of_tpn] << :day_of_tpn if record.day_of_tpn.blank?
+      REQUIRED_FIELDS.each do |field|
+        record.errors["#{field}"] << "can't be blank" if eval("record.#{field}.blank?")
+      end
     end
   end
 end
