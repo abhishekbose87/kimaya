7789f0df5ba9e2e7bd8ddd2644059fe63b7a90f2
Shailesh Patil <shailesh@joshsoftware.com>
2011-04-13 13:42:29 +0530

    adding validations for fields

--- lib/kimaya/validator.rb
+++ lib/kimaya/validator.rb
@@ -1,7 +1,7 @@
 module Kimaya
   class TPNValidator < ActiveModel::Validator
-    def validate(record)
-      options[:my_custom_key] # => "my custom value"
+    validates_each :day_of_tpn, :current_weight do |record, attr, value|
+      record.errors.add attr, 'cant be blank' if value.blank?
     end
   end
 end
