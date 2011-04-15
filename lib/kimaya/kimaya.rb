a4e94712ce059e48cd8566205fdf8521ed220990
Shailesh Patil <shailesh@joshsoftware.com>
2011-04-15 14:41:08 +0530

    initialized the accessors

--- lib/kimaya/kimaya.rb
+++ lib/kimaya/kimaya.rb
@@ -31,7 +31,6 @@ module Kimaya
       @calcium_intake = initialize_key(options, :calcium_intake, 3)
       @calcium_conc = initialize_key(options, :calcium_conc, 3)
       @administration = options.has_key?(:administration) ? options.fetch(:administration) : nil
-      return false unless self.valid?
     end
 
     def calculate_tpn 
