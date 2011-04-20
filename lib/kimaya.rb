a9388ec79141d89e3daa1ad60ca28b307d240141
Shailesh Patil <shailesh@joshsoftware.com>
2011-04-20 15:43:33 +0530

    testing

--- lib/kimaya.rb
+++ lib/kimaya.rb
@@ -3,3 +3,4 @@ require 'active_model'
 require  File.dirname(__FILE__) + '/kimaya/kimaya_core.rb'
 require  File.dirname(__FILE__) + '/kimaya/kimaya.rb'
 I18n.load_path = Dir[(File.dirname(__FILE__) + '/config/locales/*.yml').to_s]
+I18n.default_locale = :en
