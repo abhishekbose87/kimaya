8537a7dbf24089a5a2d7bc3544d84f226c41ea47
Shailesh Patil <shailesh@joshsoftware.com>
2011-04-20 15:36:43 +0530

    testing error messages

--- lib/kimaya.rb
+++ lib/kimaya.rb
@@ -2,3 +2,4 @@ require 'active_support'
 require 'active_model'
 require  File.dirname(__FILE__) + '/kimaya/kimaya_core.rb'
 require  File.dirname(__FILE__) + '/kimaya/kimaya.rb'
+I18n.load_path = Dir[(File.dirname(__FILE__) + '/config/locales/*.yml').to_s]
